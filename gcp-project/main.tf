8ef76b8c93763c0887305c2225023098cd233a65
Lukas Karlsson <lukwam@gmail.com>
2022-10-27 02:46:39 +0000

    First commit!

---
+++ gcp-project/main.tf
@@ -0,0 +1,46 @@
+variable "billing_account" {
+  description = "The alphanumeric ID of the billing account this project belongs to."
+  type        = string
+}
+variable "folder_id" {
+  description = "The numeric ID of the folder this project should be created under."
+  type        = string
+}
+variable "labels" {
+  description = "A set of key/value label pairs to assign to the project."
+  type        = map(any)
+}
+variable "project_id" {
+  description = "The project ID."
+  type        = string
+}
+variable "project_name" {
+  description = "The display name of the project."
+  type        = string
+}
+variable "services" {
+  description = "List of services to enable."
+  type        = list(string)
+  default = [
+    "bigquery.googleapis.com",
+    "bigquerymigration.googleapis.com",
+    "bigquerystorage.googleapis.com",
+    "cloudapis.googleapis.com",
+    "clouddebugger.googleapis.com",
+    "cloudtrace.googleapis.com",
+    "datastore.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "serviceusage.googleapis.com",
+    "sql-component.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+    "storage.googleapis.com",
+  ]
+}
+variable "skip_delete" {
+  description = "If true, the Terraform resource can be deleted without deleting the Project via the Google API."
+  type        = bool
+  default     = false
+}
---
+++ gcp-project/project.tf
@@ -0,0 +1,28 @@
+resource "google_project" "project" {
+  name            = var.project_name
+  project_id      = var.project_id
+  folder_id       = var.folder_id
+  billing_account = var.billing_account
+
+  labels = merge(var.labels, {
+    billing   = lower(var.billing_account)
+    tf_module = "gcp-project",
+  })
+
+  skip_delete = var.skip_delete ? true : false
+}
+output "project" {
+  value = google_project.project
+}
+
+resource "google_project_service" "services" {
+  for_each = toset(var.services)
+  project  = google_project.project.project_id
+  service  = each.key
+
+  disable_dependent_services = false
+  disable_on_destroy         = true
+}
+output "services" {
+  value = google_project_services.services
+}
